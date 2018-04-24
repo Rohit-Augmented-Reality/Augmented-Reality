bf063e50e06c1ff1d481720c01ac75cce555fc7d
Rohit <rohitnair987@gmail.com>
2018-04-24 19:00:34 -0400

    added and positioned an iron man model

--- Majin/ViewController.swift
+++ Majin/ViewController.swift
@@ -27,7 +27,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/Iron_Man/Iron_Man.scn")!
         
         // Set the scene to the view
         sceneView.scene = scene

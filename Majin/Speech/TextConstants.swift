db22ebfdcef6651d3ecd21b88ee0e9949ca21d4d
Rohit <rohitnair987@gmail.com>
2018-04-27 18:17:40 -0400

    Added text to speech with sample welcome message and touch feedback message

---
+++ Majin/Speech/TextConstants.swift
@@ -0,0 +1,17 @@
+//
+//  TextConstants.swift
+//  Majin
+//
+//  Created by Rohit Nair on 4/27/18.
+//  Copyright © 2018 Rohit Nair. All rights reserved.
+//
+
+import Foundation
+
+class TextConstants {
+    static let WelcomeMsg = "Hello! Welcome to my first A R Kit application."
+    
+    static let TouchFeedback = "How can I help you?"
+    
+    static let ConfirmPrefix = "Did you say "
+}
--- Majin/ViewController.swift
+++ Majin/ViewController.swift
@@ -6,30 +6,29 @@
 //  Copyright © 2018 Rohit Nair. All rights reserved.
 //
 
-import UIKit
-import SceneKit
 import ARKit
+import AVFoundation
+import SceneKit
+import UIKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    let synth = AVSpeechSynthesizer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // View the initial position and feature points for reference
+        // MARK: Debug - View the initial position and feature points for reference
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         
+        // MARK: Scene setup
         // Set the view's delegate
         sceneView.delegate = self
-        
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
         // Create a new scene
-        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
         let scene = SCNScene(named: "art.scnassets/Iron_Man/Iron_Man.scn")!
-        
         // Set the scene to the view
         sceneView.scene = scene
     }
@@ -55,8 +54,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
-    // MARK: - ARSCNViewDelegate
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let utterance = AVSpeechUtterance(string: TextConstants.WelcomeMsg)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        synth.speak(utterance)
+    }
     
 /*
     // Override to create and configure nodes for anchors added to the view's session.
@@ -85,6 +88,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         self.sceneView.scene.rootNode.addChildNode(newBallNode)
     }
     */
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let utterance = AVSpeechUtterance(string: TextConstants.TouchFeedback)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        synth.speak(utterance)
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
